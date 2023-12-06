ecd2da783df962eaaae4a6e7393c4d3ed18f820c
cezarszl <cezarszlfedora@gmail.com>
2023-12-06 21:20:37 +0100

    Added loading data from myApi

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setmovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cezarszlmyflix-0212aa467a8d.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((key) => {
+          return {
+            id: key._id,
+            title: key.Title,
+            image: key.ImagePath,
+            director: key.Director.Name
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
