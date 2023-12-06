e77e9532690513d6d44787684c38b2ec8e1dc1da
cezarszl <cezarszlfedora@gmail.com>
2023-12-06 21:51:58 +0100

    Added loading data from myFlixAPI

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,33 +1,28 @@
 import React from "react";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setmovies] = useState([
-    {
-      id: 1,
-      title: "The Matrix Reloaded",
-      image:
-        "https://resizing.flixster.com/iAVu-9508aPQUAZDOq_Tymo0vWw=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p31912_p_v8_bb.jpg",
-      director: "Lilly Wachowski"
-    },
-    {
-      id: 2,
-      title: "Django Unchained",
-      image:
-        "https://resizing.flixster.com/EYUgjFKEFJZ58_5O3FniOs3d3gY=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p9287010_p_v8_aa.jpg",
-      director: "Quentin Tarantino"
-    },
-    {
-      id: 3,
-      title: "Ready Player One",
-      image:
-        "https://resizing.flixster.com/_9PDK6o0bmP_TkIFvV8I8_hRTUg=/206x305/v2/https://resizing.flixster.com/CZWlGfTfgPK4K6noRQ35fiQ3V0M=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzg2MDgxODQyLWZlZjgtNGE5OC05NjUwLTU0MWVmOGFkYTA0YS53ZWJw",
-      director: "Steven Spielberg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cezarszlmyflix-0212aa467a8d.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((key) => {
+          return {
+            id: key._id,
+            title: key.Title,
+            image: key.ImagePath,
+            director: key.Director.Name
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
@@ -48,7 +43,8 @@ export const MainView = () => {
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
           }} />
-      })}
-    </div>
+      })
+      }
+    </div >
   );
 };
