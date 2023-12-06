fe41fe04f7c1d11ac38ea2b5b549a9f2d1f3e9f2
cezarszl <cezarszlfedora@gmail.com>
2023-12-06 11:41:50 +0100

    Full-Stack Immersion Exercise 3.3

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { MainView } from "./components/main-view/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
-  );
+const App = () => {
+ return <MainView />;
 };
 
 // Finds the root of your app
@@ -17,4 +14,4 @@ const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
