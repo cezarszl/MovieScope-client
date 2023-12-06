61334144453d414a74d6bfbc5e69741d41e1a7e2
cezarszl <cezarszlfedora@gmail.com>
2023-12-06 20:30:18 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,29 +5,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setmovies] = useState([
-    {
-      id: 1,
-      title: "The Matrix Reloaded",
-      image:
-        "https://resizing.flixster.com/iAVu-9508aPQUAZDOq_Tymo0vWw=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p31912_p_v8_bb.jpg",
-      director: "Lilly Wachowski"
-    },
-    {
-      id: 2,
-      title: "Django Unchained",
-      image:
-        "https://resizing.flixster.com/EYUgjFKEFJZ58_5O3FniOs3d3gY=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p9287010_p_v8_aa.jpg",
-      director: "Quentin Tarantino"
-    },
-    {
-      id: 3,
-      title: "Ready Player One",
-      image:
-        "https://resizing.flixster.com/_9PDK6o0bmP_TkIFvV8I8_hRTUg=/206x305/v2/https://resizing.flixster.com/CZWlGfTfgPK4K6noRQ35fiQ3V0M=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzg2MDgxODQyLWZlZjgtNGE5OC05NjUwLTU0MWVmOGFkYTA0YS53ZWJw",
-      director: "Steven Spielberg"
-    }
-  ]);
+  const [movies, setmovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
